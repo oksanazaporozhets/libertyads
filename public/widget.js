dbb5a86f737c472ac8ce0054f51b1f989fba6772
Alex Zaporozhets <alexzaporozhets@gmail.com>
2014-01-09 21:35:03 +0200

    widget.js adds line splitter for ads

--- public/widget.js
+++ public/widget.js
@@ -108,7 +108,7 @@ function escapeHtml(unsafe) {
 
                     getRandomSubarray(libertyAds.dataObj, 2).forEach(function (ad) {
                         var y = 0;
-                        svgData.push('<a xlink:href="' + ad.url + '" target="_top"><text x="' + x + '" y="' + y + '" font-size="16" fill="blue">' + encodeURIComponent(escapeHtml(ad.title)) + '</text></a>');
+                        svgData.push('<a xlink:href="' + ad.url + '" target="_top"><text x="' + x + '" y="' + y + '" font-size="16" text-decoration = "underline" fill="blue">' + encodeURIComponent(escapeHtml(ad.title)) + '</text></a>');
 
                         splitIntoLines(ad.text, 50).forEach(function(line){
                             y += 20;
